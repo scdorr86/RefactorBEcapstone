6fcf19b8ab34f85a80bad8fd3c69da1cce5f049a
Sean Dorris <seanc.dorris@gmail.com>
2024-04-01 16:17:11 -0500

    continued setup for controller, interfaces, services etc

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using RefactorBEcapstone.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace RefactorBEcapstone.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        protected readonly RefactorDbContext _context;
+
+        public GenericRepository(RefactorDbContext context) => _context = context;
+
+        public async Task<TEntity> AddAsync(TEntity model, bool saveChanges = true)
+        {
+            _context.Set<TEntity>().Add(model);
+            if (saveChanges)
+                await _context.SaveChangesAsync();
+            return model;
+        }
+    }
+}
---
+++ Repositories/IGenericRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace RefactorBEcapstone.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> AddAsync(TEntity model, bool saveChanges = true);
+
+    }
+}
