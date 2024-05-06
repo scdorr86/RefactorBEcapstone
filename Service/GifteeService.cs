01e819edf74ca15739789a855710f512cbb81954
Sean Dorris <seanc.dorris@gmail.com>
2024-05-06 16:39:36 -0500

    giftee controller begun

--- Service/GifteeService.cs
+++ Service/GifteeService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using RefactorBEcapstone.Giftee.Requests;
 using RefactorBEcapstone.Giftee.Responses;
 using RefactorBEcapstone.Models;
 using RefactorBEcapstone.Repositories;
@@ -22,6 +23,14 @@ namespace RefactorBEcapstone.Service
         {
             var giftees = await _gifteeRepo.GetAllAsync();
             return _mapper.Map<List<GifteeResponse>>(giftees);
+        }
+
+        public async Task<GifteeResponse> CreateGiftee(GifteeRequest request)
+        {
+            var newGiftee = _mapper.Map<Models.Giftee>(request);
+            var result = await _gifteeRepo.AddAsync(newGiftee);
+            var mappedResult = _mapper.Map<GifteeResponse>(result);
+            return mappedResult;
         }
     }
 }
